b4f4cb789c5b5b66eff53190c7e881cf8dbd8f50
Brandon Nesslage <Brandonnesslage@gmail.com>
2024-06-26 11:38:13 -0400

    Added some basic file structure and toggle capabilities for our two main components. We can add more components if needed in the future.

---
+++ src/components/board.jsx
@@ -0,0 +1,9 @@
+const Board = () => {
+  return (
+    <>
+      <h1>Heres the Board</h1>
+    </>
+  );
+};
+
+export default Board;
---
+++ src/components/gameStart.jsx
@@ -0,0 +1,9 @@
+const GameStart = () => {
+  return (
+    <>
+      <h1>Play Checkers!</h1>
+    </>
+  );
+};
+
+export default GameStart
