51fd1c339c2c66217f3c9e1a16094640bdb8c6cf
Brandon Nesslage <Brandonnesslage@gmail.com>
2024-06-28 10:03:44 -0400

    Added a board

--- src/components/gameStart.jsx
+++ src/components/gameStart.jsx
@@ -2,7 +2,7 @@ const GameStart = () => {
   return (
     <>
     {/* lets play around with a front page. This is just an example */}
-      <h1>Play Checkers!</h1>
+      <h1 className="title">Play Checkers!</h1>
     </>
   );
 };
